bedd0aeeff71211a5a33817e018d28b99b99525d
Ian P Bradley <ian.bradley@studiocrabapple.com>
2018-10-03 12:48:38 -0700

    USPS::WebTools::Tracking::Codes reworked

--- lib/USPS/WebTools/Tracking/Codes.pm
+++ lib/USPS/WebTools/Tracking/Codes.pm
@@ -7,93 +7,84 @@ use Readonly;
 
 use Exporter 'import';
 
-our @EXPORT_OK = qw(name_to_code code_to_name code_is_return_to_sender name_is_return_to_sender); 
+our @EXPORT_OK = qw(code_to_description code_is_return_to_sender code_is_attempted_notice_left); 
 our %EXPORT_TAGS = ( all => [ @EXPORT_OK ] );
 
 #
 # USPS Tracking Code/Name Lookup Tables
 #
 
-Readonly our %name_to_code_return_to_sender => (
-  return_to_sender => '09',
-  return_to_sender_no_such_number => '21',
-  return_to_sender_insufficient_address => '22',
-  return_to_sender_moved => '23',
-  return_to_sender_forward_expired => '24',
-  return_to_sender_addressee_unknown => '25',
-  return_to_sender_vacant => '26',
-  return_to_sender_unclaimed => '27',
-  return_to_sender_deceased => '28', # ;_;
-  return_to_sender_other => '29',
-  return_to_sender_not_picked_up => '31'
+Readonly our %return_to_sender => (
+  '09' => 'Return to Sender',
+  '21' => 'No Such Number',
+  '22' => 'Insufficient Address',
+  '23' => 'Moved, Left No Address',
+  '24' => 'Forward Expired',
+  '25' => 'Addressee Unknown',
+  '26' => 'Vacant',
+  '27' => 'Unclaimed',
+  '28' => 'Deceased',
+  '29' => 'Other',
+  '31' => 'Not Picked Up'
 );
 
-Readonly our %code_to_name_return_to_sender => map {
-  $name_to_code_return_to_sender{$_}, $_
-} keys %name_to_code_return_to_sender;
-
-Readonly our %name_to_code => (
-  manifest_acknowledgement => 'MA',
-  out_for_delivery => 'OF',
-  sorting_processing_complete => 'PC',
-  delivered => '01',
-  delivery_status_not_updated => 'DX',
-  attempted_notice_left => '02',
-  notice_left => '52',
-  receptacle_blocked => '53',
-  receptacle_full_item_oversized => '54',
-  no_secure_location_available => '55',
-  no_authorized_recipient_available => '56',
-  accept_or_pickup_by_carrier => '03',
-  truck_manifest_shipment_acceptance => 'TM',
-  refused => '04',
-  undeliverable_as_addressed => '05',
-  forwarded => '06',
-  arrival_at_unit => '07',
-  missent => '08',
-  %name_to_code_return_to_sender,
-  processed => '10',
-  passive_acceptance_shipment_acceptance => 'PA',
-  dead_letter => '11',
-  arrival_at_pickup_point => '14',
-  mis_shipped => '15',
-  available_for_pickup => '16',
-  picked_up_by_agent => '17',
-  dc_evs_arrive => '19',
-  recieved_at_opening_unit => '41',
-  usps_handoff_to_shipping_partner => '42',
-  picked_up => '43',
-  customer_recall => '44',
-  business_closed => '51',
-  picked_up_by_shipping_partner => '80',
-  arrived_at_shipping_partner_facility => '81',
-  departed_shipping_partner_facility => '82',
-  departed_sort_facility => 'EF',
-  departed_post_office => 'SF'
+Readonly our %attempted_notice_left => (
+  '02' => 'Attempted / Notice Left',
+  '52' => 'Notice Left',
+  '53' => 'Receptacle Blocked',
+  '54' => 'Receptacle Full / Item Oversized',
+  '55' => 'No Secure Location Available',
+  '56' => 'No Authorized Recipient Available'
 );
 
-Readonly::Hash our %code_to_name => map {
-  $name_to_code{$_}, $_
-} keys %name_to_code;
+Readonly our %codes => (
+  %return_to_sender,
+  %attempted_notice_left,
+  'MA' => 'Manifest Acknowledgement',
+  'OF' => 'Out for Delivery',
+  'PC' => 'Sorting / Processing Complete',
+  '01' => 'Delivered',
+  'DX' => 'Delivery Status Not Updated',
+  '03' => 'Accept or Pickup (by carrier)',
+  'TM' => 'Truck manifest, provided as "Shipment Acceptance"',
+  '04' => 'Refused',
+  '05' => 'Undeliverable as Addressed',
+  '06' => 'Forwarded',
+  '07' => 'Arrival at Unit',
+  '08' => 'Missent',
+  '10' => 'Processed',
+  'PA' => 'Passive Acceptance, provided as "Shipment Acceptance"',
+  '11' => 'Dead Letter',
+  '14' => 'Arrival at Pickup Point',
+  '15' => 'Mis-shipped',
+  '16' => 'Available for Pickup',
+  '17' => 'Picked Up By Agent',
+  '19' => 'DC eVS Arrive',
+  '41' => 'Received at Opening Unit (Reserved for Open & Distribute)',
+  '42' => 'USPS Handoff to Shipping Partner',
+  '43' => 'Picked Up',
+  '44' => 'Customer Recall',
+  '51' => 'Business Closed',
+  '80' => 'Picked Up by Shipping Partner',
+  '81' => 'Arrived Shipping Partner Facility',
+  '82' => 'Departed Shipping Partner Facility',
+  'EF' => 'Departed Sort Facility',
+  'SF' => 'Departed Post Office' 
+);
 
-sub name_to_code {
-  my $name = shift;
-  return $name_to_code{$name}
-}
-
-sub code_to_name {
+sub code_to_description {
   my $code = shift;
-  return $code_to_name{$code}
+  return $codes{$code}
 }
 
 sub code_is_return_to_sender {
   my $code = shift;
-  return 1 if $code_to_name_return_to_sender{$code}
+  return 1 if $return_to_sender{$code}
 }
 
-sub name_is_return_to_sender {
-  my $name = shift;
-  return 1 if $name_to_code_return_to_sender{$name}
+sub code_is_attempted_notice_left {
+  my $code = shift;
+  return 1 if $attempted_notice_left{$code}
 }
 
 1;
